d2a79512bbca88f98a3b613c1756faa20ad2f4bd
shubhamsavarn <shubham.savarn99@gmail.com>
2022-10-23 15:44:32 +0530

    components/navbar

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import react from 'react';
+import Navbar from '../components/Navbar';
+const Home = () =>{
+    return(
+    <div>
+        <h1>Shubham</h1>
+        <Navbar/>
+    </div>
+    )
+}
+export default Home
