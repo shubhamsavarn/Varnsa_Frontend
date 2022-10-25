aa8962e531b72fab201149cb1cabd22d1b7a005a
shubhamsavarn <shubham.savarn99@gmail.com>
2022-10-25 23:29:18 +0530

    component/navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,79 @@
-import React from 'react'
+import styled from 'styled-components';
+import SearchIcon from '@mui/icons-material/Search';
+import Badge from '@mui/material/Badge';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+const Container = styled.div`
+height : 60px;
+`
+const Wrapper = styled.div`
+padding : 10px 20px;
+display: flex;
+justify-content : space-between;
+align-item :center;
+`
+const Left = styled.div`
+flex:1;
+display: flex;
+align-item: center;
+`
+const Language = styled.span`
+font-size : 14px;
+cursor:pointer;
+`
+const SearchContainer = styled.div`
+display: flex;
+align-items:center;
+margin-left:25px;
+padding: 5px;
+`
+const Input = styled.input`
+border:none;
+`
+
+const Right = styled.div`
+flex:1;
+display:flex;
+align-item:center;
+justify-content: flex-end;
+
+`
+const MenuItem = styled.div`
+font-size: 14px;
+cursor:pointer;
+`
+const Center = styled.div`
+flex:1;
+text-align: center;
+`
+const Logo = styled.h1`
+font-weight :  bold;
+
+`
 
 const Navbar = () => {
   return (
-    <div>
-      navbar
-    </div>
+    <Container>
+    <Wrapper>
+        <Left>
+        <Language>
+            EN
+        </Language>
+        <SearchContainer>
+            <Input/>
+            <SearchIcon/>
+        </SearchContainer>
+        </Left>
+        <Center><Logo>VARNSA.</Logo></Center>
+        <Right><MenuItem>REGISTER</MenuItem>
+        <MenuItem>SIGN IN</MenuItem>
+        <MenuItem>
+        <Badge badgeContent={4} color="secondary">
+        <ShoppingCartOutlinedIcon/>
+        </Badge>
+        </MenuItem>
+        </Right>
+    </Wrapper>
+    </Container>
   )
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,7 @@
-import react from 'react';
 import Navbar from '../components/Navbar';
 const Home = () =>{
     return(
     <div>
-        <h1>Shubham</h1>
         <Navbar/>
     </div>
     )
